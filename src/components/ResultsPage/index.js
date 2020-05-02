cca09d038b03a51dbd9dcabf396052eee77cca21
Michel Moraes <michel@nitdesign.com.br>
2020-05-02 19:17:43 +0200

    Styling page and fixing prop types

--- src/components/ResultsPage/index.js
+++ src/components/ResultsPage/index.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ResultsTable from "../ResultsTable";
-import "./style.sass";
 
 const ResultsPage = () => {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState({});
 
   return (
     <section className="results-page">
@@ -12,15 +11,17 @@ const ResultsPage = () => {
         <h2 className="results-page__headline">EuroJackpot Result & Winning Numbers</h2>
         <Row>
           <Col md={6}>
-            <h4>EuroJackpot Results for Friday 01 May 2020</h4>
+            <h4 className="results-page__subline">
+              <span>EuroJackpot</span> Results for Friday 01 May 2020
+            </h4>
             <ul className="results-balls">
               <li className="results-balls__item">6</li>
               <li className="results-balls__item">11</li>
               <li className="results-balls__item">12</li>
               <li className="results-balls__item">21</li>
               <li className="results-balls__item">41</li>
-              <li className="results-balls__item">1</li>
-              <li className="results-balls__item">2</li>
+              <li className="results-balls__item results-balls__item--extra">1</li>
+              <li className="results-balls__item results-balls__item--extra">2</li>
             </ul>
           </Col>
         </Row>
@@ -29,16 +30,16 @@ const ResultsPage = () => {
             <ResultsTable results={results} />
           </Col>
           <Col md={4}>
-            <div className="infoContainer">
+            <div className="side-bar">
               <h6>The EuroJackpot numbers for {`01.05.2020`}</h6>
               <p>
-                <strong>
+                <span>
                   The {`424th`}th draw for the EuroJackpot was held on {`01.05.2020`}
-                </strong>
+                </span>
                 , as usual at 9pm in Helsinki.
               </p>
             </div>
-            <div className="infoContainer">
+            <div className="side-bar">
               <h6>EuroJackpot numbers for {`01.05.2020`}</h6>
               <p>
                 The balls used for the draw are made of a synthetic polymer, softer than ping-pong
